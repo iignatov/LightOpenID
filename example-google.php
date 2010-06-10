73f65740498974629e0d1763ac8c0ad5fe162ecd
mewp <mewp151@gmail.com>
2010-06-10 22:01:38 +0200

    Added an example showing how to authenticate with Google acoount.

---
+++ example-google.php
@@ -0,0 +1,24 @@
+<?php
+# Logging in with Google accounts requires special treatment, so this example shows how to do it.
+require 'openid.php';
+try {
+    if(!isset($_GET['openid_mode'])) {
+        if(isset($_GET['login'])) {
+            $openid = new LightOpenID;
+            $openid->identity = 'https://www.google.com/accounts/o8/id';
+            header('Location: ' . $openid->authUrl(true));
+        }
+?>
+<form action="?login" method="post">
+    <button>Login with Google</button>
+</form>
+<?php
+    } elseif($_GET['openid_mode'] == 'cancel') {
+        echo 'User has canceled authentication!';
+    } else {
+        $openid = new LightOpenID;
+        echo 'User ' . ($openid->validate() ? $_GET['openid_identity'] . ' has ' : 'has not ') . 'logged in.';
+    }
+} catch(ErrorException $e) {
+    echo $e->getMessage();
+}
