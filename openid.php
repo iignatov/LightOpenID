2efc8cf218642d1d3c7bbc5c37e20de1d38d1d91
mewp <mewp151@gmail.com>
2010-09-27 23:28:36 +0200

    Another series of bugfixes:
    Fixed issue #6 - the library is now trying to avoid errors generated by fopen().
    $openid->identity is now converted to a string automatically, to avoid another kind of warnings.
    When issuing HEAD requests, claimed_id will now follow redirections (by Location header).
    
    LightOpenID now requires PHP >= 5.1.2, so please report if it's a problem for anyone and they really can't upgrade.

--- openid.php
+++ openid.php
@@ -39,7 +39,7 @@
  * To get the values, use $openid->getAttributes().
  *
  *
- * The library requires PHP 5 with http/https stream wrappers enabled..
+ * The library requires PHP >= 5.1.2 with http/https stream wrappers enabled..
  * @author Mewp
  * @copyright Copyright (c) 2010, Mewp
  * @license http://www.opensource.org/licenses/mit-license.php MIT
@@ -78,7 +78,7 @@ class LightOpenID
     {
         switch ($name) {
         case 'identity':
-            if (strlen($value = trim($value))) {
+            if (strlen($value = trim((String) $value))) {
                 if (preg_match('#^xri:/*#i', $value, $m)) {
                     $value = substr($value, strlen($m[0]));
                 } elseif (!preg_match('/^(?:[=@+\$!\(]|https?:)/i', $value)) {
@@ -109,9 +109,35 @@ class LightOpenID
             return $this->trustRoot;
         }
     }
+    
+    /**
+     * Checks if the server specified in the url exists.
+     *
+     * @param $url url to check
+     * @return true, if the server exists; false otherwise
+     */
+    function hostExists($url)
+    {
+        if (strpos($url, '/') === false) {
+            $server = $url;
+        } else {
+            $server = @parse_url($url, PHP_URL_HOST);
+        }
+        
+        if (!$server) {
+            return false;
+        }
+        
+        return !!gethostbynamel($server);
+    }
+
 
     protected function request($url, $method='GET', $params=array())
     {
+        if(!$this->hostExists($url)) {
+            throw new ErrorException('Invalid request.');
+        }
+        
         $params = http_build_query($params, '', '&');
         switch($method) {
         case 'GET':
@@ -149,6 +175,9 @@ class LightOpenID
 
             $url = $url . ($params ? '?' . $params : '');
             $headers_tmp = get_headers ($url);
+            if(!$headers_tmp) {
+                return array();
+            }
             
             # Parsing headers.
             $headers = array();
@@ -156,6 +185,22 @@ class LightOpenID
                 $pos = strpos($header,':');
                 $name = strtolower(trim(substr($header, 0, $pos)));
                 $headers[$name] = trim(substr($header, $pos+1));
+                
+                # Following possible redirections. The point is just to have
+                # claimed_id change with them, because get_headers() will
+                # follow redirections automatically.
+                # We ignore redirections with relative paths.
+                # If any known provider uses them, file a bug report.
+                if($name == 'location') {
+                    if(strpos($headers[$name], 'http') === 0) {
+                        $this->claimed_id = $headers[$name];
+                    } elseif($headers[$name][0] == '/') {
+                        $parsed_url = parse_url($this->claimed_id);
+                        $this->claimed_id = $parsed_url['scheme'] . '://'
+                                          . $parsed_url['host']
+                                          . $headers[$name];
+                    }
+                }
             }
 
             # And restore them.
