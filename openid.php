0fa024a1282cb5cfd7c77443b66ba88eed40dd10
LarsAndreLandas <landas@gmail.com>
2011-10-08 18:09:22 +0200

    Fix Issue 48: In php 5.4 the function get_magic_quotes_gpc() is deprecated and has been removed.

--- openid.php
+++ openid.php
@@ -682,7 +682,7 @@ class LightOpenID
             # wants to verify. stripslashes() should solve that problem, but we can't
             # use it when magic_quotes is off.
             $value = $this->data['openid_' . str_replace('.','_',$item)];
-            $params['openid.' . $item] = get_magic_quotes_gpc() ? stripslashes($value) : $value;
+            $params['openid.' . $item] = function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc() ? stripslashes($value) : $value;
 
         }
 
